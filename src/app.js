426c0b4c35aac3b19f2f7bd1d52554ac8396bd4d
mikaeeld <41543031+Mikaeeld@users.noreply.github.com>
2022-05-13 12:15:55 +0200

    Restructure routes match file conventions

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const mainRouter = require('./routes/main')
+const mainRouter = require('./routes/main.routes')
 
 const app = express()
 
