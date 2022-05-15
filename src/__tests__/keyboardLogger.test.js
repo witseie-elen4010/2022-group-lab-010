b4fdafdfbae52b0abc936bd9c8f5c60d6f8ac1e1
Jeniel Jinabhai <jenieljinabhai@gmail.com>
2022-05-15 19:18:03 +0200

    buttons log to text area

--- src/__tests__/keyboardLogger.test.js
+++ src/__tests__/keyboardLogger.test.js
@@ -1,25 +1,19 @@
 'use strict'
-const handlers = require('../routes/handlers')
-
-
+/* eslint-env jest */
 const keyboard = require('../public/scripts/keyboardLogger')
 
-//import functionToExecute from '../public/scripts/keyboardLogger'
-
-
-
+// import functionToExecute from '../public/scripts/keyboardLogger'
 
 describe('Test Button component', () => {
   it('Test click event', () => {
+    // const mockCallBack = jest.fn()
 
-
-    const mockCallBack = jest.fn();
-
-    //const button = shallow((<button onClick={mockCallBack}>a</button>));
-    //const keyElement = 
-    //button.find('buttonA').simulate('click')
-    expect(keyboard.functionToExecute("a")).toBe("a")
-  });
-});
-
-
+    // const button = shallow((<button onClick={mockCallBack}>a</button>));
+    // const keyElement =
+    // button.find('buttonA').simulate('click')
+    expect(keyboard.functionToExecute('a')).toBe('a')
+  })
+  it('Test click event', () => {
+    expect(keyboard.functionToExecute('ab')).toBe('a')
+  })
+})
--- src/public/scripts/keyboardLogger.js
+++ src/public/scripts/keyboardLogger.js
@@ -1,50 +1,66 @@
 'use strict'
 
- const  functionToExecute = function ( letter) {
+const functionToExecute = function (letter) {
   console.log(letter)
-  return letter;
 
-} 
-module.exports = {functionToExecute}
+  if (letter.length > 1) {
+    console.log('error button length is too long ')
+    letter = letter.substring(0, 1) // Take the first element
+  }
 
+  const message = document.getElementById('guess')
 
-/* 
+  message.value += letter
+
+  // console.log(message.value)
+  const keyArr = document.getElementById('buttonQ')
+  for (let i = 0; i < keyArr.length; i++) {
+    keyArr[i].addEventListener('click', function () {
+      document.getElementById('response').value += letter
+    })
+  }
+  return letter
+}
+
+const removeLetter = function () {
+  const message = document.getElementById('guess')
+  message.value = message.value.substring(0, message.value.length - 1)
+}
+module.exports = { functionToExecute }
+
+/*
 const buttonQ = document.getElementById('buttonQ')
 buttonQ.addEventListener('click', function () {
     console.log("q")
- 
+
 }, false)
 
 const buttonW = document.getElementById('buttonW')
 buttonW.addEventListener('click', function () {
     console.log("W")
- 
+
 }, false)
 
 const buttonE = document.getElementById('buttonE')
 buttonE.addEventListener('click', function () {
     console.log("Y")
- 
+
 }, false)
 
 const buttonR = document.getElementById('buttonR')
 buttonR.addEventListener('click', function () {
     console.log("Y")
- 
+
 }, false)
 
 const buttonT = document.getElementById('buttonT')
 buttonT.addEventListener('click', function () {
     console.log("Y")
- 
+
 }, false)
 
 const buttonY = document.getElementById('buttonY')
 buttonT.addEventListener('click', function () {
     console.log("Y")
- 
+
 }, false) */
-
-
-
-
