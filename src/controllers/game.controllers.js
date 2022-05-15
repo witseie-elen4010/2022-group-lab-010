ad5fee502a76b70d837eb813873a3f6b7b95069f
Thomas Makins <thomas.makins@protonmail.com>
2022-05-15 21:14:11 +0200

    Added random word

--- src/controllers/game.controllers.js
+++ src/controllers/game.controllers.js
@@ -2,9 +2,31 @@
 const fs = require('fs')
 const path = require('path')
 
+// Generate random number with seed
+const xmur3 = (str) => {
+  let number = 0
+  const length = str.length
+  for (let i = 0; i < length; i++) {
+    number += str.charCodeAt(i)
+  }
+  let seed = number
+  for (let i = 0; i < 1000; i++) {
+    seed = seed * 16807 % 2147483647
+  }
+  return seed / 2147483647
+}
+
 const getCorrectWord = () => {
-  // For now the only correct word is SMART
-  return 'SMART'
+  const date = new Date().toISOString().split('T')[0]
+
+  const file = fs.readFileSync(path.join(__dirname, '..', 'models', 'dictionary.json'))
+  const dict = JSON.parse(file)
+  const len = dict.length
+
+  const index = parseInt(xmur3(date) * len)
+  const word = dict[index]
+  console.log(word)
+  return word.toUpperCase()
 }
 
 const wordIsValid = (word) => {
