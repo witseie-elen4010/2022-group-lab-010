230f3cc2373dac278eecdc99f2f3fefb1d3274de
Jeniel Jinabhai <jenieljinabhai@gmail.com>
2022-05-19 22:37:39 +0200

    timer triggers end game screen

--- src/public/scripts/timer.js
+++ src/public/scripts/timer.js
@@ -1,35 +1,57 @@
+/* import {gameEnd} from '/'; */
 document.getElementById('timer').innerHTML =
-    00 + ":" + 15;
-    let pause = false
+    00 + ":" + 05;
+let pause = false
 startTimer();
 
 
 
 function startTimer() {
 
-if(pause == false)
-{
-    var presentTime = document.getElementById('timer').innerHTML;
-    var timeArray = presentTime.split(/[:]+/);
-    var m = timeArray[0];
-    var s = checkSecond((timeArray[1] - 1));
-    if (s == 59) { m = m - 1 }
-    if (m < 0) {
-        return
+    if (pause == false) {
+        var presentTime = document.getElementById('timer').innerHTML;
+        var timeArray = presentTime.split(/[:]+/);
+        var m = timeArray[0];
+        var s = checkSecond((timeArray[1] - 1));
+        if (s == 59) { m = m - 1 }
+        if (m < 0) {
+            return
+        }
+
+        document.getElementById('timer').innerHTML =
+            m + ":" + s;
+        //console.log(m)
+        setTimeout(startTimer, 1000);
     }
 
-    document.getElementById('timer').innerHTML =
-        m + ":" + s;
-    //console.log(m)
-    setTimeout(startTimer, 1000);
-}
 
-    
 
     if (m == 0 && s == 0) {
         console.log('TImer is up')
+        fetch('/api/correct')
+            .then(function (response) {
+                if (response.ok) { return response.json() } // Return the response parse as JSON
+                else { throw 'Failed to load correct word: response code invalid!' }
+            })
+
+            .then(function (data) {
+                const x = document.getElementById('Game Result')
+                x.innerHTML = ` You ran out of time. Please try again` //The correct word was ${data}` // Dont show the correct word when you run out of time
+            })
+
+            .catch(function (e) { // Process error for request
+                alert(e) // Displays a browser alert with the error message.
+            })
+
+        const y = document.getElementById('submit Guess')
+        y.setAttribute('class', 'btn btn-primary')
+        y.setAttribute('data-bs-toggle', 'modal')
+        y.setAttribute('data-bs-target', '#staticBackdrop')
+        y.innerHTML = 'complete'
+        y.click()
+
     }
-   /*  console.log(pause) */
+    /*  console.log(pause) */
 
 }
 
@@ -49,4 +71,4 @@ const pauseTimer = function () {
     startTimer()
 
 }
-module.exports = {pauseTimer}
+module.exports = { pauseTimer }
