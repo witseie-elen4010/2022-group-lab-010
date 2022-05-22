3eba6169ebc8bbf3b7b9143336e9303c98f25adf
shivun-wits <80011093+shivun-wits@users.noreply.github.com>
2022-05-22 21:54:29 +0200

    added MongoDB and Mongoose ADR and Implementation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const express = require('express')
+require('dotenv').config({ path: '../.env' })
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const mainRouter = require('./routes/main.routes')
 
@@ -14,6 +16,13 @@ app.use('/', mainRouter)
 app.use('/cdn', express.static('public'))
 
 module.exports = app
+
+// Configure Database connection
+const mongoDB = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const port = process.env.PORT || 3000
 app.listen(port)
