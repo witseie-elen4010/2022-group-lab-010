765361411dec17bde9de1bef930be9632e4d53d5
Thomas Makins <thomas.makins@protonmail.com>
2022-05-29 09:07:36 +0200

    Formatted code to meet standards. with standard --fix

--- src/controllers/guesses.controllers.js
+++ src/controllers/guesses.controllers.js
@@ -88,8 +88,7 @@ const colourCodeGuess = async (req, res) => {
   for (let i = 0; i < out.colour.length; i++) {
     if (out.colour[i] === 'green') {
       score = score + 4 * (5 - turn) ** 2
-    }
-    else if (out.colour[i] === 'yellow') {
+    } else if (out.colour[i] === 'yellow') {
       score = score + (5 - turn) ** 2
     }
   }
